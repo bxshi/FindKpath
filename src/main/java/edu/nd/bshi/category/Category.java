3ba292625083a90eaa272dc94664fb27da715395
bxshi <baoxu.shi@gmail.com>
2013-12-01 16:17:54 -0500

    move mysql loading from main to this class

--- src/main/java/edu/nd/bshi/category/Category.java
+++ src/main/java/edu/nd/bshi/category/Category.java
@@ -5,10 +5,8 @@ import org.apache.logging.log4j.Logger;
 import org.apache.lucene.util.ArrayUtil;
 import org.neo4j.helpers.collection.ReverseArrayIterator;
 
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import java.sql.*;
+import java.util.*;
 
 
 public class Category {
@@ -28,6 +26,22 @@ public class Category {
             categoryInstance = new Category();
         }
         return categoryInstance;
+    }
+
+    public static int getTopCategory(int node) {
+        CategoryNode categoryNode = Category.getNode(node);
+        if (categoryNode != null) {
+            while (categoryNode.getHeight() != 1) {
+                categoryNode = categoryNode.getParent();
+            }
+            return categoryNode.getIndex();
+        } else {
+            return 0;
+        }
+    }
+
+    public static int getTopCategory(CategoryNode node) {
+        return getTopCategory(node.getIndex());
     }
 
     public static boolean putNode(CategoryNode node, int parentIndex) {
@@ -71,4 +85,87 @@ public class Category {
         ArrayUtil.quickSort(heights);
         return new ReverseArrayIterator<Integer>(heights);
     }
+
+    public static LinkedList<LinkedList<Integer>> getPathsCategories(LinkedList<LinkedList<Integer>> paths, Statement stat) {
+        ResultSet res;
+        LinkedList<LinkedList<Integer>> catePaths = new LinkedList<LinkedList<Integer>>();
+        try {
+            for (LinkedList<Integer> path : paths) {
+                catePaths.add(new LinkedList<Integer>());
+                for (int node : path) {
+                    int cate = 0;
+                    res = stat.executeQuery("SELECT " +
+                            "parentID, childID, `left(A.path, locate(',', A.path)-1)` as mainCate " +
+                            "FROM wikipedia.clel, wikipedia.articleToMainTopicCategories where childID=" + node + " and `stop`=" + node);
+                    while (res.next()) {
+                        int id = res.getInt("parentID");
+                        int topCategory = res.getInt("mainCate");
+                        logger.info("parentID " + id + " topCate " + topCategory + " actual topCate " + Category.getTopCategory(id));
+                        if (topCategory == Category.getTopCategory(id)) {
+                            cate = Category.getNode(id).getHeight() > Category.getNode(cate).getHeight() ?
+                                    id : cate;
+                        }
+                    }
+                    catePaths.getLast().add(cate);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return catePaths;
+    }
+
+    public static boolean loadCategoriesFromMySQL(String url, String user, String passwd) {
+        Connection conn = null;
+        Statement stat = null;
+        ResultSet rs = null;
+        try {
+            conn = DriverManager.getConnection(url, user, passwd);
+            stat = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+            stat.setFetchSize(Integer.MIN_VALUE);
+            rs = stat.executeQuery("SELECT path FROM wikipedia.pathsFromMainTopicCategories where namespace=14");
+            int i = 0;
+            while (rs.next()) {
+
+                if (i++ % 50000 == 0 && logger.isInfoEnabled()) {
+                    logger.info("Category tree constructing... " + i + " nodes processed");
+                }
+
+                String path = rs.getString(1);
+                String[] pathList = path.split(",");
+                int index = Integer.parseInt(pathList[pathList.length - 1]);
+                int height = pathList.length;
+                int parent = pathList.length > 1 ? Integer.parseInt(pathList[pathList.length - 2]) : 0;
+                CategoryNode categoryNode = new CategoryNode(index, height);
+                Category.putNode(categoryNode, parent);
+            }
+
+        } catch (SQLException e) {
+            logger.fatal("MySQL error! " + e.toString());
+            e.printStackTrace();
+            return false;
+        } catch (Exception e) {
+            logger.fatal("Construction error! " + e.toString());
+            e.printStackTrace();
+            return false;
+        } finally {
+            try {
+                if (rs != null)
+                    rs.close();
+                if (stat != null) {
+                    stat.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                logger.fatal("MySQL error! " + e.toString());
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
